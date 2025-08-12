0ef5707da68d46e601d142c45bd126507cd98d05
Jules <julesmoreno66@gmail.com>
2025-08-12 13:19:38 +0800

    first commit: java practice

---
+++ GuessingGame.java
@@ -0,0 +1,54 @@
+public class GuessingGame {
+  Boolean isGameOver = false;
+  int numberGuess;
+  Player p1 = new Player();
+  Player p2 = new Player();
+  Player p3 = new Player();
+
+  void gameStart() {
+    Boolean p1Guessed;
+    Boolean p2Guessed;
+    Boolean p3Guessed;
+
+    numberGuess = (int) (Math.random() * 9);
+
+    while (!isGameOver) {
+      p1.guessed();
+      p2.guessed();
+      p3.guessed();
+      p1Guessed = p1.numGuess == numberGuess;
+      p2Guessed = p2.numGuess == numberGuess;
+      p3Guessed = p3.numGuess == numberGuess;
+
+      System.out.println("Number to guess is " + numberGuess);
+      System.out.println("Player one guessed " + p1.numGuess);
+      System.out.println("Player two guessed " + p2.numGuess);
+      System.out.println("Player three guessed " + p3.numGuess);
+      if (p1Guessed || p2Guessed || p3Guessed) {
+        isGameOver = true;
+        System.out.println("We have a Winner!");
+        System.out.println("Player one got it right? " + p1Guessed);
+        System.out.println("Player two got it right? " + p2Guessed);
+        System.out.println("Player three got it right? " + p3Guessed);
+        System.out.println("Game is over.");
+      } else {
+        System.out.println("Players will have to try again.");
+      }
+    }
+  }
+}
+
+class Player {
+  int numGuess;
+
+  void guessed() {
+    numGuess = (int) (Math.random() * 9);
+  }
+}
+
+class GameLauncher {
+  public static void main(String[] args) {
+    GuessingGame launcher = new GuessingGame();
+    launcher.gameStart();
+  }
+}
---
+++ Movie.java
@@ -0,0 +1,28 @@
+public class Movie {
+  String title;
+  String genre;
+  int rating;
+
+  void playIt() {
+    System.out.println("Playing the movie");
+  }
+
+  void displayMovie() {
+    System.out.println(title);
+    System.out.println(genre);
+    System.out.println(rating);
+  }
+}
+
+class MovieTestDrive {
+  public static void main(String[] args) {
+    Movie one = new Movie();
+    one.title = "The Sand";
+    one.genre = "Horror";
+    one.rating = 4;
+    one.playIt();
+    one.displayMovie();
+  }
+}
+
+// Build a guessing game like the example in the book.
