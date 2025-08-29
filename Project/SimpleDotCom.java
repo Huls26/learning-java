702037346fa59ab7f932ab644bae1faa6a49650a
Jules <julesmoreno66@gmail.com>
2025-08-29 10:33:46 +0800

    created Project folder and SimpleDotCom template

---
+++ Project/SimpleDotCom.java
@@ -0,0 +1,20 @@
+package Project;
+
+public class SimpleDotCom {
+  int[] locationCells;
+  int numOfHits = 0;
+
+  String checkYourSelf(String userGuessed) {
+
+  }
+
+  void setLocationCells() {
+
+  }
+}
+
+class SimpleDotComTestDrive {
+  public static void main(String[] args) {
+
+  }
+}
