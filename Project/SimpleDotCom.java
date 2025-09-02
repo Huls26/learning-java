19476025c24755ca4a56d3713b2fad6bb8400f5b
Jules <julesmoreno66@gmail.com>
2025-09-02 10:57:44 +0800

    start new project

--- Project/SimpleDotCom.java
+++ Project/SimpleDotCom.java
@@ -4,30 +4,8 @@ public class SimpleDotCom {
   private int[] locationCells;
   private int numOfHits = 0;
 
-  int getNumOfHits() {
-    return numOfHits;
-  }
-
   String checkYourSelf(String userGuess) {
-    int integerGuess = Integer.parseInt(userGuess);
-    int x = 0;
 
-    while (x < 3) {
-      int currentValue = locationCells[x];
-
-      if (currentValue == integerGuess) {
-        numOfHits = numOfHits + 1;
-
-        if (numOfHits == 3) {
-          return "kill";
-        } else {
-          return "hit";
-        }
-      }
-      x = x + 1;
-    }
-
-    return "miss";
   }
 
   // Pick randomNum between 0 and 4
@@ -35,32 +13,11 @@ public class SimpleDotCom {
   // store randomNum + index
   // Result: locationCells contains 3 consecutive numbers
   void setLocationCells() {
-    locationCells = new int[3];
-    int randomNum = (int) (Math.random() * 5);
-    int x = 0;
-
-    while (x < 3) {
-      locationCells[x] = randomNum + x;
-      x = x + 1;
-    }
   }
 }
 
 class SimpleDotComTestDrive {
   public static void main(String[] args) {
-    SimpleDotCom testRun = new SimpleDotCom();
-    int numberOfGuess = 0;
 
-    testRun.setLocationCells();
-
-    while (testRun.getNumOfHits() < 3) {
-      String userGuess = System.console().readLine("Enter a number ");
-      String returnValue = testRun.checkYourSelf(userGuess);
-      System.out.println(returnValue);
-      numberOfGuess = numberOfGuess + 1;
-      if (returnValue == "kill") {
-        System.out.println("You took " + numberOfGuess + " guess");
-      }
-    }
   }
 }
