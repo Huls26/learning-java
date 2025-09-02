bdacf78ed8220b983a247b832c57697930da24d4
Jules <julesmoreno66@gmail.com>
2025-09-02 11:17:50 +0800

    update test code

--- Project/SimpleDotCom.java
+++ Project/SimpleDotCom.java
@@ -22,7 +22,6 @@ class SimpleDotComTestDrive {
 
     String userGuess = "2";
     String testResult = "Failed";
-    String result = dot.checkYourSelf(userGuess);
 
     if (dot.checkYourSelf(userGuess).equals("hit")) {
       testResult = "Passed";
