c4da88ee702584777fede33d5a6caced1249de99
Jules <julesmoreno66@gmail.com>
2025-09-10 14:55:07 +0800

    created

---
+++ Project/DotCom.java
@@ -0,0 +1,38 @@
+package Project;
+
+import java.util.ArrayList;
+
+public class DotCom {
+  private ArrayList<String> locationCells;
+
+  // private numOfHits = 0;
+
+  public void setLocationCells(ArrayList<String> locations) {
+    locationCells = locations;
+  }
+
+  public String checkYourSelf(String userGuess) {
+    int idx = locationCells.indexOf(userGuess);
+
+    if (locationCells.contains(userGuess)) {
+      locationCells.remove(idx);
+
+      return locationCells.isEmpty() ? "kill" : "hit";
+    }
+
+    return "miss";
+  }
+}
+
+class DotComTestDrive {
+  public static void main(String[] args) {
+    DotCom d = new DotCom();
+    ArrayList<String> locationList = new ArrayList<String>();
+
+    locationList.add("1");
+    locationList.add("2");
+    locationList.add("3");
+
+    d.setLocationCells(locationList);
+  }
+}
